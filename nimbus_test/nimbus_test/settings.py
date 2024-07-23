544fec296403e941bbedad033dddef1f1429f0e1
Marcelo Cavalcanti de Medeiros Sobrinho <ithomer@hotmail.com>
2024-07-23 00:10:08 -0300

    Update settings.py

--- nimbus_test/nimbus_test/settings.py
+++ nimbus_test/nimbus_test/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'marcelo.cavalcanti.092@ufrn.edu.br'
-EMAIL_HOST_PASSWORD = 'xsxj ouim ddyn miup'
+EMAIL_HOST_USER = 'seu email aqui'
+EMAIL_HOST_PASSWORD = 'sua senha aqui'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
